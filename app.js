6c8f9344d383561a306606400173d2bba9e6bee1
galorbir000 <74693113+galorbir000@users.noreply.github.com>
2021-01-18 14:24:06 -0800

    editing fiels

--- app.js
+++ app.js
@@ -4,13 +4,63 @@ const toggleOff = document.querySelectorAll('.off');
 const toggleOn = document.querySelector(".on");
 const alertBanner = document.getElementById("alert");
 
-const trafficCanvas = document.getElementById("traffic-chart");
-const dailyCanvas = document.getElementById("daily-chart");
+const traficCanvas = document.querySelector("#trafic-chart");
+const dailyCanvas = document.getElementById("dailyCanvas");
 const mobileCanvas = document.getElementById("mobile-chart");
 
-const user = document.getElementById("user-field");
-const message = document.getElementById("message-field");
-const send = document.getElementById("send");
+
+
+alertBanner.innerHTML = 
+`
+<div class="alert-banner">
+<p><strong>Alert:</strong> You have <strong>6</strong> overdue tasks
+to complete</p>
+<p class="alert-banner-close">x</p>
+</div>
+`;
+
+alertBanner.addEventListener("click", e => {
+  const element = e.target;
+  if( element.classList.contains("alert-banner-close" )) {
+    alert.style.display = "none";
+  }
+});
+
+
+const trafficData = {
+  labels: ["16-22", "23-29", "30-5", "6-12", "13-19", "20-26", "27-3",
+  "4-10", "11-17", "18-24", "25-31"],
+  datasets: [{
+  data: [500, 1000,1500, 2000, 1500, 1750, 1250, 1850, 2250, 1500,
+  2500],
+  backgroundColor: 'rgba(116, 119, 191, .3)',
+  borderWidth: 1,
+  }]
+  };
+
+  let trafficOptions = {
+    aspectRatio: 2.5,
+    animation: {
+    duration: 0
+    },
+    scales: {
+    yAxes: [{
+    ticks: {
+    beginAtZero:true
+    }
+    }]
+    },
+    legend : {
+    display: false
+    }
+    };
+
+    let trafficChart = new Chart(traficCanvas, {
+      type: 'line',
+      data:trafficData ,
+      options: trafficOptions
+      });
+   
 
 const dailyData = {
   labels: ["S", "M", "T", "W", "T", "F", "S"],
@@ -40,39 +90,7 @@ let dailyChart = new Chart(dailyCanvas, {
   options: dailyOptions
   });
 
-let trafficData = {
-  labels: ["16-22", "23-29", "30-5", "6-12", "13-19", "20-26", "27-3",
-  "4-10", "11-17", "18-24", "25-31"],
-  datasets: [{
-  data: [750, 1250, 1000, 2000, 1500, 1750, 1250, 1850, 2250, 1500,
-  2500],
-  backgroundColor: 'rgba(116, 119, 191, .3)',
-  borderWidth: 1,
-  }]
-  };
 
-  let trafficOptions = {
-    aspectRatio: 2.5,
-    animation: {
-    duration: 0
-    },
-    scales: {
-    yAxes: [{
-    ticks: {
-    beginAtZero:true
-    }
-    }]
-    },
-    legend : {
-    display: false
-    }
-    };
-
-    let trafficChart = new Chart(trafficCanvas, {
-      type: "line",
-      data: trafficData,
-      options: trafficOptions
-    });
     
     const mobileData = {
       labels: ["Desktop", "Tablet", "Phones"],
@@ -103,23 +121,9 @@ let mobileChart = new Chart(mobileCanvas, {
   data: mobileData,
   options: mobileOptions
   });
-
-alert.innerHTML = 
-`
-<div class="alert-banner">
-<p><strong>Alert:</strong> You have <strong>6</strong> overdue tasks
-to complete</p>
-<p class="alert-banner-close">x</p>
-</div>
-`;
-
-alertBanner.addEventListener("click", e => {
-  const element = e.target;
-  if( element.classList.contains("alert-banner-close" )) {
-    alert.style.display = "none";
-  }
-});
-
+  const user = document.getElementById("user-field");
+  const message = document.getElementById("message-field");
+  const send = document.getElementsByClassName("send");
 send.addEventListener("click", () => {
   // ensure user and message fields are filled out
   if (user.value === "" && message.value === "") {
@@ -133,17 +137,17 @@ send.addEventListener("click", () => {
     }
 });
 
-emailToggle.addEventListener('click', () =>  {
-  if (emailToggle.checked) {
-    toggleOff[0].style.display = 'none';
-  } else {
-    toggleOff[0].style.display = '';
-  }
-});
-profileToggle.addEventListener("click", () => {
-    if (profileToggle.checked) {
-        toggleOff[1].style.display = "none";
-    } else {
-        toggleOff[1].style.display = "";
-    }
-});
+// emailToggle.addEventListener('click', () =>  {
+//   if (emailToggle.checked) {
+//     toggleOff[0].style.display = 'none';
+//   } else {
+//     toggleOff[0].style.display = '';
+//   }
+// });
+// profileToggle.addEventListener("click", () => {
+//     if (profileToggle.checked) {
+//         toggleOff[1].style.display = "none";
+//     } else {
+//         toggleOff[1].style.display = "";
+//     }
+// });
